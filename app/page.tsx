1f1a8195955f0ab00300b3e90bae3e102909540c
samuelegen <samuelegennaro@gmail.com>
2025-02-05 17:04:36 +0100

    home page e navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,22 @@
+import styles from "/app/page.module.scss";
 import Image from "next/image";
+const params =
+	{
+    immagine: "/immagine_home.jpg",
+		title: "Benvenuto/a nel mio sito web",
+		description:
+			"Mi chiamo Samuele e sono un Front-End Developer appassionato alla creazione di esperienze web interattive e reattive. Con un forte focus sull'usabilità e sul design, mi occupo di sviluppare soluzioni moderne utilizzando tecnologie come HTML, CSS, JavaScript, React e framework come Next con cui ho realizzato proprio questo sito. Credo che la chiave per un progetto di successo risieda nella cura dei dettagli e nell'attenzione alle richieste dell'utente finale.",
+	}
+
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+	return (
+		<div className={styles.container}>
+			<main className={styles.main}>
+        <Image src={params.immagine} width={200} height={200} alt="immagine_home" className={styles.image_options}/>
+				<h1>{params.title}</h1>
+				<p>{params.description}</p>
+			</main>
+		</div>
+	);
 }
---
+++ components/Molecoles/Navbar.tsx
@@ -0,0 +1,44 @@
+// components/Navbar.tsx
+"use client"
+import Link from "next/link";
+import React from "react";
+import styles from "/components/Molecoles/Navbar.module.scss"
+
+interface NavItem {
+	label: string;
+	href: string;
+}
+
+interface NavbarProps {
+	items?: NavItem[];
+}
+
+const Navbar: React.FC<NavbarProps> = ({ items }) => {
+	const navItems: NavItem[] = items || [
+		{ label: "Home", href: "/" },
+		{ label: "About", href: "/about" },
+		{ label: "Projects", href: "/projects" },
+		{ label: "Contact", href: "/contact" },
+	];
+
+	return (
+		<nav className={styles.navbar} aria-label="Main Navigation">
+			<div className={styles.container}>
+                <Link href="/" className={styles.logo}>
+                Samuele Gennaro 
+                </Link>
+				<ul className={styles.navList}>
+					{navItems.map((item) => (
+						<li key={item.href}>
+							<Link href={item.href} className={styles.navLink}>
+								{item.label}
+							</Link>
+						</li>
+					))}
+				</ul>
+			</div>
+		</nav>
+	);
+};
+
+export default Navbar;
